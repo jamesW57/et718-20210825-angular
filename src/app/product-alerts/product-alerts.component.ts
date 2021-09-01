649b5331f7be53bd2706e4a512ae4a63efe8bb45
jamesW57 <james.wang57.qcc@gmail.com>
2021-09-01 11:08:00 -0400

    4 files modified

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Input } from '@angular/core';
+import { Output, EventEmitter } from '@angular/core';
 import { Product } from '../products';
 
 @Component({
@@ -7,7 +8,21 @@ import { Product } from '../products';
   templateUrl: './product-alerts.component.html',
   styleUrls: ['./product-alerts.component.css']
 })
-export class ProductAlertsComponent implements OnInit {
+export class ProductAlertsComponent {
+  @Input() product: Product|undefined;
+  @Output() notify = new EventEmitter();
+}
+export class ProductListComponent {
+  product=products;
+  share(){
+    window.alert('The product has been shared!');
+  }
+
+  onNotify() {
+    window.alert('You will be notified when the product goes on sale');
+  }
+}
+implements OnInit {
   @Input() product!: Product;
   constructor() { }
 
